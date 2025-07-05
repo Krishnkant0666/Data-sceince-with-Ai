e0771cd457e9fb51b4d78ab371ce1f3b47ba8042
Krishnkant0666 <shuklakrishnkant22@gmail.com>
2025-07-05 10:29:10 +0530

    Python.

---
+++ Basic.py
@@ -0,0 +1,231 @@
+# print("hello hiii kese ho ")
+# print("hello hiii kese ho")
+# print("hello hiii kese ho")
+# print("hello hiii kese ho")
+
+
+# a = 10
+# print(a)
+# 3 = 3
+# @ = 34
+# __ = a 
+# _a = 3
+# __a = 23
+# a8v = 5 
+
+# python keyword 
+# print = 10
+# len = 15
+# import = 1203
+# if = 5
+# elif = 12
+
+
+
+# age = 22
+# Age = 23
+# print(age)
+
+# _a = 3
+# __a = 23
+
+
+# name = "Ritik"
+# print(name)
+# print("type of str :-",type(name)) # check the data type 
+# print("len of str:-",len(name))
+
+
+# # indexing
+# print(name[2])
+# print(name[4])
+
+# # Slicing 
+# print(name[0:3])
+
+## operation 
+
+
+
+# uper_case =name.upper()
+# print(uper_case)
+
+# last = "KUMAR"
+# lower_Case =last.lower()
+# print(lower_Case)
+
+
+
+# title_name=name.title()
+# print(title_name)
+# cap_name=name.capitalize()
+# print(cap_name)
+# print(name.count("r"))
+
+# name = "ritik"
+# last = "kumar"
+# print(name+" "+last)
+
+
+# reverse >>>>>>>>>>>>>>>>>>>home work
+# name = "ritik"
+# name = name[::-1]
+# print(name)
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..list >>>>>>>>>>>>>>>>>>>>>>>>>
+# mumtable 
+# dupl 
+# hetro
+# indexing 
+# slicing 
+# array or list
+# print("my first list:-", my_list) 
+# print("type of my list:-",type(my_list))
+# print("len of my lis:-t ",len(my_list))
+# print(my_list.count(2))
+# print("indexing = ",my_list[3])
+# print("slicing = ",my_list[0:8])
+# # print(my_list[::-1])
+# my_list.reverse()
+# print(my_list)
+
+
+# my_list = [1,2,34,54,5,6,6,6,"rohit", "diya", 2.2, 2 ]
+# my_list.append(100)
+# print(my_list)
+
+# my_list = [1,2,34,54,5,6,6,6, 2.2, 2 ]
+# my_list1 = [1,2,34,54,5,6,6,6, 2.2, 2 ]
+# add
+# sub 
+# Mul 
+# divde 
+# print(my_list*2)
+# my_list[0]=1000
+# print(my_list)
+
+# my_list.remove(2)
+# my_list.pop(2)
+# my_list.index(0)
+# my_list.pop()
+# my_list.
+# print(my_list)
+
+# copy_list =my_list.copy()
+# print(copy_list)
+# my_list.sort()
+# print(my_list)
+# my_list.clear() easy to learn 
+# my_list.insert(0, 1000)
+# print(my_list)
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  tuple   <<<<<<<<<<<<<<<<<<<<<
+# tpl = (1,2,3,4,5)
+# print("my first tpl:-",tpl)
+# print("type of tpl:-",type(tpl))
+# print("len of tpl:-",len(tpl))
+# print(tpl.count(2))
+# print("indexing with tpl:-",tpl.index(2))
+# print(tpl[1])
+# print(tpl[::-1])
+# tpl.append(100)
+# print(tpl)
+
+# name = 1,2,5,25,54,5,5 
+# print(name)
+# print(type(name))
+# name = ("ritik","diya","rohit")
+# print(name)
+# print(type(name))
+# riitik_name , diya_name , rohit_name = name
+
+
+### tuple unpacking 
+# num = (1,2,3,4)
+# a ,b,c = num
+# print(a)
+# print(b)
+# print(c)
+
+# num = (1,2)
+# a ,b,c = num
+# print(a)
+# print(b)
+# print(c)
+
+
+
+
+# tpl2 = (1,34,3,4,5,6,66,7,7,77,7,7)
+# # print(tpl1+tpl2)
+# # print(tpl1*2)
+# print(tpl1*tpl2)
+# print(tpl1/tpl2)
+
+# tpl1 = (1,34,3,4,5,6,66,7,7,77,7,7)
+# tpl1 = list(tpl1)
+# tpl1.append(100)
+# print(tuple(tpl1))
+
+# home work 
+# task = ["ritik","diya","rohit", (1,2,[3 ,6],4)]
+# task = tuple(task)
+# print(task)
+
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   dict  >>>>>>>>>>>>>>>>
+# dict 
+# mutable
+# key value pair
+# key must be unique 
+# key must be immutable 
+
+# name, age, address = keys
+# ritk, 22, kolkata = values
+# print("my first dict:-",my_dict)
+# print("type of my dict:-",type(my_dict))
+# print("len of my dict:-",len(my_dict))
+# (my_dict[4]) # it is not possible to access index in dict 
+# print(my_dict["name"])
+# print(my_dict['age'])
+# print(my_dict['address'])
+# my_dict = {"name":"ritik","age":22,"address":"kolkata"}
+# print(my_dict.keys())
+# print(my_dict.values())
+# print(my_dict.items())
+
+# my_dict['mobile_number']=9166780327
+# print(my_dict)
+
+
+# update funtion 
+
+# my_dict.update({"mobile_number":9166780327})
+# print(my_dict)
+# copy 
+# deep copy 
+# copy_dict=my_dict.copy()
+# print(copy_dict)
+# my_dict.clear()
+# my_dict.
+
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>  set   >>>>>>>>>>>>>>>>>>>>>>>
+# set 
+# mutable
+# unorder 
+# no duplicate values
+# no index
+# no slicing
+# sat = {1,2,3,4,5,5,5}
+# print("my first set:-",sat)
+# print("return type of set:-",type(sat))
+# print("len of set:-",len(sat))
+# sat.add(6)
+# print(sat)
+# sat.remove(6)
+# print(sat)
